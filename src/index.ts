0d192baa62972a9f23ce1655bf5ead73310b3c4f
kosuke-17 <tamutamu17@icloud.com>
2022-02-20 12:55:20 +0900

    GETリクエストのハンドリング

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,21 @@ const PORT = process.env.PORT || 4000;
 /**
  * todo一覧情報を返す.
  *
+ * @remarks クエリパラメーターを用いたフィルタリングを実装
+ *
  * @returns - todo一覧情報
  */
 app.get("/api/todos", (req: Request, res: Response, next: NextFunction) => {
-  return res.json(todos);
+  console.log(req.query);
+
+  // クエリパラメーターが存在しなければ、全てのtodoを返す
+  if (!req.query.completed) {
+    return res.json(todos);
+  }
+
+  // クエリパラメーターはstringのため比較対象はStr型
+  const completed = req.query.completed === "true";
+  res.json(todos.filter((todo) => todo.completed === completed));
 });
 
 app.listen(PORT, () => console.log("サーバー起動"));
