66f4715221f2747dcc10d87eac1c86b6a7996874
kosuke-17 <tamutamu17@icloud.com>
2022-02-20 13:31:47 +0900

    POSTリクエストのとエラーのハンドリング

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 /**
+ * ミドルウェアを利用
+ */
+// postリクエストのリクエストボディを受け付けるため
+app.use(express.json());
+
+/**
  * todo一覧情報を返す.
  *
  * @remarks クエリパラメーターを用いたフィルタリングを実装
@@ -16,8 +22,6 @@ const PORT = process.env.PORT || 4000;
  * @returns - todo一覧情報
  */
 app.get("/api/todos", (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.query);
-
   // クエリパラメーターが存在しなければ、全てのtodoを返す
   if (!req.query.completed) {
     return res.json(todos);
@@ -27,5 +31,40 @@ app.get("/api/todos", (req: Request, res: Response, next: NextFunction) => {
   const completed = req.query.completed === "true";
   res.json(todos.filter((todo) => todo.completed === completed));
 });
+
+/**
+ * todoの追加.
+ *
+ * @returns - 追加したtodoの内容
+ */
+let id = 2;
+app.post("/api/todos", (req: Request, res: Response, next: NextFunction) => {
+  const { title } = req.body;
+  if (typeof title !== "string" || !title) {
+    const err = new Error("タイトルがありません");
+    return next(err);
+  }
+
+  const todo = { id: (id += 1), title, completed: false };
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+/**
+ * エラーハンドリングのミドルウェア
+ *
+ * @returns - ステータスコード
+ * @returns - エラーメッセージ
+ */
+app.use(
+  (
+    err: { statusCode: number; message: string },
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    res.status(500).json({ error: err.message });
+  }
+);
 
 app.listen(PORT, () => console.log("サーバー起動"));
