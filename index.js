55e81dc5d01d17d164c9e2e27a2af88d3ab57a1a
kosuke-17 <tamutamu17@icloud.com>
2022-02-20 11:47:24 +0900

    サーバー起動処理作成

---
+++ index.js
@@ -0,0 +1,25 @@
+import http from "http";
+
+const todos = [
+  { id: 1, title: "ネーム", completed: false },
+  { id: 2, title: "下書き", completed: true },
+];
+
+const server = http.createServer((req, res) => {
+  //リクエストのURLやHTTPメソッドに応じてレスポンスを返す
+  if (req.url === "/api/todos") {
+    if (req.method === "GET") {
+      // 全todoをJSON形式で返す
+      res.setHeader("Content-Type", "application/json");
+      return res.sendDate(JSON.stringify(todos));
+    }
+    // GETメソッド以外のメソッドだったら405
+    res.statusCode = 405;
+  } else {
+    // 条件式のurlでリクエストが来なければNot Found
+    res.statusCode = 404;
+  }
+  res.end();
+});
+
+server.listen(3000, () => console.log("サーバー起動中"));
